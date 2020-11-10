3f9217c6a6c5bd87d7f3a51e8f25208aa57fd073
Lucas Porfírio <lucas.porfirio1@hotmail.com>
2020-11-10 19:57:08 -0300

    a1

---
+++ Aula 06-10/Php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        define("SENHA_DATABASE";"123456");
+        $nome="Lucas Porfírio"; 
+    ?>
+    <h1><?= "Pagina do".$nome ?></h1>
+    <?php 
+    echo 'Lucas Porfírio'
+    ?>
+</body>
+</html>
