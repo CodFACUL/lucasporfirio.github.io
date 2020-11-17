c4b24657a125aeece352e75967d60a406628443a
Lucas Porfírio <lucas.porfirio1@hotmail.com>
2020-11-16 21:47:53 -0300

    Corpocalculadora

--- calculadora/index.php
+++ calculadora/index.php
@@ -6,6 +6,39 @@
     <title>Document</title>
 </head>
 <body>
+
+<form method="POST">
+        <div id="calculadora">
+            <input type="text" value="entrou" name="entrou" style="display:none;">
+            <input type="text" disabled id="visor" >
+            <div id="primeira">
+                <input type="submit" value="7" name="valor" class="botao">
+                <input type="submit" value="8" name="valor" class="botao">
+                <input type="submit" value="9" name="valor" class="botao">
+                <input type="submit" value="/" name="valor" class="botao">
+            </div>
+            <div id="segunda">
+                <input type="submit" value="4" name="valor" class="botao">
+                <input type="submit" value="5" name="valor" class="botao">
+                <input type="submit" value="6" name="valor" class="botao">
+                <input type="submit" value="*" name="valor" class="botao">
+            </div>
+            <div id="terceira">
+                <input type="submit" value="1" name="valor" class="botao">
+                <input type="submit" value="2" name="valor" class="botao">
+                <input type="submit" value="3" name="valor" class="botao">
+                <input type="submit" value="-" name="valor" class="botao">
+            </div>
+            <div id="quarta">
+                <input type="submit" value="CE" name="valor" class="botao">
+                <input type="submit" value="0" name="valor" class="botao">
+                <input type="submit" value="=" name="valor" class="botao">
+                <input type="submit" value="+" name="valor" class="botao">
+            </div>
+        </div>
+    </form>
+</body>
+
     
 </body>
 </html>
