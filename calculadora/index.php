aacdd3cd0ff7689789304eb94ab4b907c1216b20
Lucas Porfírio <lucas.porfirio1@hotmail.com>
2020-11-16 22:22:44 -0300

    php e mudanças

--- calculadora/index.php
+++ calculadora/index.php
@@ -7,34 +7,44 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+session_start();
 
+if (empty($_SESSION['calc'])) {
+    $_SESSION['calc'] = '';
+  }
+if ($_POST['digito'] == 'CE') {
+        unset($_SESSION['calc']);
+        $digito = 0;
+      }
+?>
 <form method="POST">
-        <div id="calculadora">
-            <input type="text" value="entrou" name="entrou" style="display:none;">
-            <input type="text" disabled id="visor" >
-            <div id="primeira">
-                <input type="submit" value="7" name="valor" class="botao">
-                <input type="submit" value="8" name="valor" class="botao">
-                <input type="submit" value="9" name="valor" class="botao">
-                <input type="submit" value="/" name="valor" class="botao">
+        <div id="corpo">
+            <input type="text" value="entra" name="entra" style="display:none;">
+            <input type="text" disabled id="resultado" value="<?= !empty($digito) ? $digito : 0 ?>">
+            <div id="linhaa">
+                <input type="submit" value="7" name="digito" class="botao">
+                <input type="submit" value="8" name="digito" class="botao">
+                <input type="submit" value="9" name="digito" class="botao">
+                <input type="submit" value="/" name="digito" class="botao">
             </div>
-            <div id="segunda">
-                <input type="submit" value="4" name="valor" class="botao">
-                <input type="submit" value="5" name="valor" class="botao">
-                <input type="submit" value="6" name="valor" class="botao">
-                <input type="submit" value="*" name="valor" class="botao">
+            <div id="linhab">
+                <input type="submit" value="4" name="digito" class="botao">
+                <input type="submit" value="5" name="digito" class="botao">
+                <input type="submit" value="6" name="digito" class="botao">
+                <input type="submit" value="*" name="digito" class="botao">
             </div>
-            <div id="terceira">
-                <input type="submit" value="1" name="valor" class="botao">
-                <input type="submit" value="2" name="valor" class="botao">
-                <input type="submit" value="3" name="valor" class="botao">
-                <input type="submit" value="-" name="valor" class="botao">
+            <div id="linhac">
+                <input type="submit" value="1" name="digito" class="botao">
+                <input type="submit" value="2" name="digito" class="botao">
+                <input type="submit" value="3" name="digito" class="botao">
+                <input type="submit" value="-" name="digito" class="botao">
             </div>
-            <div id="quarta">
-                <input type="submit" value="CE" name="valor" class="botao">
-                <input type="submit" value="0" name="valor" class="botao">
-                <input type="submit" value="=" name="valor" class="botao">
-                <input type="submit" value="+" name="valor" class="botao">
+            <div id="linhad">
+                <input type="submit" value="CE" name="digito" class="botao">
+                <input type="submit" value="0" name="digito" class="botao">
+                <input type="submit" value="=" name="digito" class="botao">
+                <input type="submit" value="+" name="digito" class="botao">
             </div>
         </div>
     </form>
