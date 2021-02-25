548739076f45a84bc7074c61bcbddca2c4c7df53
lucas <lucas.porfirio@unidavi.edu>
2021-02-24 21:29:31 -0300

    sla

--- Aula01/AtividadeAula01/index.php
+++ Aula01/AtividadeAula01/index.php
@@ -4,11 +4,12 @@ require('./lib/Link.class.php');
 require('./lib/Li.class.php');
 require('./lib/Ul.class.php');
 
-$link = new Link();
+/*$link = new Link();
 $link->url='http://unidavi.edu.br'; //modo extenso// //modo1//
 $link->txt = 'Unidavi';
 $link->target="_blank";
-$link->class ='btn';
+$link->class ='btn';*/
+$link = new Link('http://unidavi.edu.br','Unidavi',"_blank",'btn');
 
 echo    $link->getLink();
 
@@ -16,7 +17,7 @@ $link2 = new Link('http://google.com.br','Google',"_blank",'btn');// modo abrevi
 
 echo    $link2->getLink();
 
-echo= new Link('http://google.com.br','Google',"_blank",'btn');// modo abreviado// //modo2//
+echo new Link('http://google.com.br','Google',"_blank",'btn');// modo abreviado// //modo2//
 
 $ul = new Ul();
 $li1 = new Li('item 1','azul');
