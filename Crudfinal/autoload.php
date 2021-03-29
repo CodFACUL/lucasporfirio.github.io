a405e0287ea4d369cbd3335811703349c819d7f2
lucas <lucas.porfirio@unidavi.edu.br>
2021-03-28 23:20:38 -0300

    dormir

--- Crudfinal/autoload.php
+++ Crudfinal/autoload.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function($className) {
-    $file = $_SERVER["DOCUMENT_ROOT"].'/lucasporfirio.github.io/Aula01/AtividadeAula01/lib/' . $className . '.class.php';
+    $file = $_SERVER["DOCUMENT_ROOT"].'/lucasporfirio.github.io/Crudfinal/lib/' . $className . '.class.php';
     if (file_exists($file)) {
         require_once $file;
     }
