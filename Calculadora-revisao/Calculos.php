fbfdfc69d24ff5b04473192c30d4e1b62fbee4d7
lucas <lucas.porfirio@unidavi.edu>
2021-05-12 21:49:10 -0300

    Jogar pra arquivo funcao

--- Calculadora-revisao/Calculos.php
+++ Calculadora-revisao/Calculos.php
@@ -1,48 +1,146 @@
 <?php 
     class Calculos{
 
+        public $val1;
+        public $val2;
 
-public function Soma($int1,$int2){
-    $_SESSION['botao']= (string)$int1+(string)$int2;
-    $_SESSION['operador']=$_POST['botao'];
-    $_SESSION['limpar']='limpar';
-    $_SESSION['valor']= $_SESSION['botao'];
-    $_SESSION['entrou']='entrou';
-    print_r($_SESSION['limpar']);
-    print_r($_SESSION['botao']);
+
+
+
+        function Soma($val1,$val2){
+            $val1=(double)$_SESSION['valor1'];
+            $val2=(double)$_SESSION['valor2'];
+            $_SESSION['valor1']=$val1+$val2;
+            $_SESSION['display']='';
+            $_SESSION['valor2']='';
+            if($_POST['botao']!='='){
+                $_SESSION['operador']=$_POST['botao'];
+            }else{
+                $_SESSION['operador']='N';
+                $_SESSION['display']=$_SESSION['valor1'];
+            }
+        
+        }
+        function Subtrai($val1,$val2){
+            $val1=(double)$_SESSION['valor1'];
+            $val2=(double)$_SESSION['valor2'];
+            $_SESSION['valor1']=$val1-$val2;
+            $_SESSION['display']='';
+            $_SESSION['valor2']='';
+            if($_POST['botao']!='='){
+                $_SESSION['operador']=$_POST['botao'];
+            }else{
+                $_SESSION['operador']='N';
+                $_SESSION['display']=$_SESSION['valor1'];
+            }
+        
+        }
+        
+        function Multiplica($val1,$val2){
+            $val1=(double)$_SESSION['valor1'];
+            $val2=(double)$_SESSION['valor2'];
+            $_SESSION['valor1']=$val1*$val2;
+            $_SESSION['display']='';
+            $_SESSION['valor2']='';
+            if($_POST['botao']!='='){
+                $_SESSION['operador']=$_POST['botao'];
+            }else{
+                $_SESSION['operador']='N';
+                $_SESSION['display']=$_SESSION['valor1'];
+            }
+        
+        }
+        
+        function Divide($val1,$val2){
+            $val1=(double)$_SESSION['valor1'];
+            $val2=(double)$_SESSION['valor2'];
+            $_SESSION['valor1']=$val1/$val2;
+            $_SESSION['display']='';
+            $_SESSION['valor2']='';
+            if($_POST['botao']!='='){
+                $_SESSION['operador']=$_POST['botao'];
+            }else{
+                $_SESSION['operador']='N';
+                $_SESSION['display']=$_SESSION['valor1'];
+            }
+        
+        }
+        
+        function logica(){
+        if($_POST['botao']=='C'){
+            unset($_SESSION['display']);
+            unset($_SESSION['valor1']);
+            unset($_SESSION['valor2']);
+            unset($_SESSION['operador']);
+        }
+        if(empty($_SESSION['operador'])){
+            $_SESSION['operador']='';
+        }
+        if(empty($_SESSION['display'])){
+            $_SESSION['display']='';
+        }
+        if(empty($_SESSION['valor1'])){
+            $_SESSION['valor1']='';
+        }
+        if(empty($_SESSION['valor2'])){
+            $_SESSION['valor2']='';
+        }
+         if(isset($_POST['botao'])){
+            if($_POST['botao']!='-' and $_POST['botao']!='+'
+                                    and $_POST['botao']!='*' 
+                                    and $_POST['botao']!='/'
+                                    and $_POST['botao']!='='
+                                    and $_POST['botao']!='C'){
+                                            if($_SESSION['operador']=='N'){
+                                                $_SESSION['display']=$_POST['botao'];
+                                                $_SESSION['valor1']='';
+                                                $_SESSION['operador']='';
+                                            }else{
+                                            $_SESSION['display'].=$_POST['botao'];}
+                                    }
+            if($_POST['botao']=='-' or $_POST['botao']=='+'
+                                    or $_POST['botao']=='*' 
+                                    or $_POST['botao']=='/'
+                                    or $_POST['botao']=='='){
+                                        if ($_SESSION['valor1']==''){
+                                        $_SESSION['valor1']=$_SESSION['display'];
+                                        $_SESSION['display']='';
+                                        $_SESSION['operador']=$_POST['botao'];
+                                        }else if($_SESSION['valor2']==''){
+                                            if($_SESSION['operador']=='N'){
+                                                $_SESSION['operador']=$_POST['botao'];}
+                                           else{
+                                                $_SESSION['valor2']=$_SESSION['display'];
+                                                $_SESSION['display']='';
+                                                    }
+                                            if($_SESSION['operador']=='+'){
+                                                Soma($_SESSION['valor1'],$_SESSION['valor2']);
+                                            }else if($_SESSION['operador']=='-'){
+                                                Subtrai($_SESSION['valor1'],$_SESSION['valor2']);
+                                            }else if($_SESSION['operador']=='*'){
+                                                Multiplica($_SESSION['valor1'],$_SESSION['valor2']);
+                                            }else if($_SESSION['operador']=='/'){
+                                                Divide($_SESSION['valor1'],$_SESSION['valor2']);
+                                            }
+                                        }
+                                       
+        
+                                    }
+        
+        
+        
+        
+        
+        
+         }
+        
+        
+
+
+
+
+
+ }
 }
 
-public function Subtrai($int1,$int2){
-    $_SESSION['botao']= (string)$int1-(string)$int2;
-                                            $_SESSION['operador']=$_POST['botao'];
-                                            $_SESSION['limpar']='limpar';
-                                            $_SESSION['valor']= $_SESSION['botao'];
-                                            $_SESSION['entrou']='entrou';
-                                            print_r($_SESSION['limpar']);
-                                            print_r($_SESSION['botao']);
-}
-
-public function Multiplica($int1,$int2){
-    $_SESSION['botao']= (string)$int1*(string)$int2;
-    $_SESSION['operador']=$_POST['botao'];
-    $_SESSION['limpar']='limpar';
-    $_SESSION['valor']= $_SESSION['botao'];
-    $_SESSION['entrou']='entrou';
-    print_r($_SESSION['limpar']);
-    print_r($_SESSION['botao']);
-}
-
-public function Divide($int1,$int2){
-    $_SESSION['botao']= (string)$int1/(string)$int2;
-    $_SESSION['operador']=$_POST['botao'];
-    $_SESSION['limpar']='limpar';
-    $_SESSION['valor']= $_SESSION['botao'];
-    $_SESSION['entrou']='entrou';
-    print_r($_SESSION['limpar']);
-    print_r($_SESSION['botao']);
-}
-
-
-
-    }
 ?>
