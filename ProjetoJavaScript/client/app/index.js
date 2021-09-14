7ffcf73c17c72f1fec4782d102ba1fbab9cf479c
lucas <lucas.porfirio@unidavi.edu>
2021-09-13 21:11:08 -0300

    ProjetoJavaScript

---
+++ ProjetoJavaScript/client/app/index.js
@@ -0,0 +1,9 @@
+var campos = [document.querySelector('#data'),document.querySelector('#valor'),document.querySelector('#quantidade')]
+
+console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit',function(event){
+    var tr=document.createElement('tr');
+})  ;
