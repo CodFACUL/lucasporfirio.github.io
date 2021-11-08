8b23aade9dffe8136d78780989405a57916a788c
João Augusto Scheneider <Jascheneider@gmail.com>
2021-11-08 19:43:18 -0300

    Criado tela de registro

---
+++ FrontJavaScript/Login/register.js
@@ -0,0 +1,29 @@
+function register(){
+    const email = document.getElementById('email');
+    const senha = document.getElementById('senha');
+    const nome = document.getElementById('nome');
+    const confirmSenha = document.getElementById('confirmSenha');
+    
+    const headers = {
+        "Content-Type": "application/json",                                                                                                
+        "Access-Control-Origin": "*"
+    }
+    const dados = {
+        email: email.value,
+        senha: senha.value,
+        nome: nome.value,
+        confirmSenha: confirmSenha.value
+    }
+    fetch('http://localhost:8000/api/auth/register',{
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(dados)
+            })
+            .then(function(response){
+                return response.json()
+            })
+            .then(function(response){
+                localStorage.setItem('token',response.access_token)
+            })
+
+}
