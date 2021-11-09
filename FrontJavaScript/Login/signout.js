4d25504d3abcd1c3642d99b981fcfe040c09528a
lucas <lucas.porfirio@unidavi.edu>
2021-11-08 21:28:23 -0300

    ok

--- FrontJavaScript/Login/signout.js
+++ FrontJavaScript/Login/signout.js
@@ -1,3 +1,6 @@
 function signout(){
     
-}
+    localStorage.removeItem('token');
+    window.location.replace('file:///C:/xampp/htdocs/lucasporfirio.github.io/FrontJavaScript/Login/login.html');
+
+    }
